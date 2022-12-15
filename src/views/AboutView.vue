2818ec50ff8c6399221fc186fd6074b953065e7c
goohayami <goohayami@gmail.com>
2022-12-16 00:41:56 +0900

    pokesvは

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
+  <h2>電気タイプの弱点</h2>
   <div class="about">
-    <h1>This is an about page</h1>
+    <table border="1">
+      <tr>
+        <td>効果</td>
+        <td>対象タイプ</td>
+      </tr>
+      <tr>
+        <td>効果はばつぐん</td>
+        <td>じめん</td>
+      </tr>
+      <tr>
+        <td>効果はいまひとつ</td>
+        <td>でんき/はがね/ひこう</td>
+      </tr>
+    </table>
+  </div>
+  <p>ooooo</p>
+  <div>
+    <div id="dis-container">
+      <img class="discription" src="../pic/denki-jimen.png" alt="" />
+      <p>ooooo</p>
+    </div>
+    <div id="dis-container">
+      <img class="discription" src="../pic/denki-dragon.png" alt="" />
+      <p>ooooo</p>
+    </div>
   </div>
 </template>
+
+<style>
+.about {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#dis-container {
+  margin-top: 1rem;
+}
+
+.discription {
+  width: 90%;
+}
+</style>
